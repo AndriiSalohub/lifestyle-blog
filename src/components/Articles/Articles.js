f215486a5c11c2423f67d7b0452eeabe8b77a30f
Andrii <aominedaikivs@gmail.com>
2022-02-22 19:51:52 +0200

    add header

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -8,9 +8,30 @@ export const Activities = () => {
       <main className="main">
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-md-3">
-            <div className="main-beach">
+            <div className="main-beach article">
               <div className="main-beach-description description">
                 Aenean ligula molstie viverra fermentum
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <div className="main-smoking article">
+              <div className="main-smoking-description description">
+                Vitae adipiscing turpis aenean rutrum leo aliquet
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <div className="main-river article">
+              <div className="main-river-description description">
+                Suspen disse pellen tesque dui proin cursus magna
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <div className="main-desert-home article">
+              <div className="main-desert-home-description description">
+                Proin dui sodales imperdiet sit sapien fames ac luctus
               </div>
             </div>
           </div>
